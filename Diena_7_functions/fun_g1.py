39ffd741c689c56e2b07199ecd8a9e5cd1eb0df7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-07 18:43:29 +0300

    default values in functions

--- Diena_7_functions/fun_g1.py
+++ Diena_7_functions/fun_g1.py
@@ -45,4 +45,38 @@ print(adder(adder(5, 20), 50))
 
 
 def mult(a, b):
+    # there could be some sanitization and validation here
     return a * b
+
+
+big_result = mult(adder(5, 10), mult(2, 20))
+print(big_result)
+
+
+def big_calc(a, b, c):
+    result = mult(adder(a, b), adder(b, c))  # (a+b)*(b+c)
+    return result
+
+
+result = big_calc(2, 3, 4)
+print(result)
+
+# we can add default values to parameters/argument
+
+# sane defaults
+
+
+def lazy_pow(a=10, b=2, pretty_print=False):
+    result = a**b
+    if pretty_print:
+        print(f"Cool {a=} to the power of {b=} is {result}")
+
+    return result
+
+
+print(lazy_pow(5, 3))
+print(lazy_pow(5))
+print(lazy_pow())
+print(lazy_pow(pretty_print=True))
+# could change order but generally not recommended
+print(lazy_pow(pretty_print=True, b=4, a=10))
