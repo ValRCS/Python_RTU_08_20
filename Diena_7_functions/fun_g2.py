b17cb32bf190e1db14267cc7171c702c3de95de8
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-08 18:43:19 +0300

    fun ievads g2

--- Diena_7_functions/fun_g2.py
+++ Diena_7_functions/fun_g2.py
@@ -8,16 +8,25 @@ my_favorite_drink = "beer"  # global variable
 def eat(food, drink="kefirs"):  # drink is "kefirs" by default
     # print("Hello")
     print(f"Let's eat {food}")
+    cal = calc_calories(food)
+    print(f"{food=} and {cal=} not bad!")
     # best to avoid using globals directly
     print(f"Let's drink some {drink}")
     # print("Let's leave")
     # i could do more stuff inside eat function
     # food arguments stops living here
+    # cal also stops living here
 # function is finished here
 
 
-# eat("potatoes")
-# eat("ice cream Sunday")
+def calc_calories(my_food):
+    calories = len(my_food) * 100  # the more letters themore calories
+    # print(f"Wow {calories=}") lets avoid side-effects
+    # could even do return len(my_food) * 100
+    return calories
+# function stops and returns whatever is after return
+
+
 def main():
     # the main action happens here
     favorite = "ice cream"
