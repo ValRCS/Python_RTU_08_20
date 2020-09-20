2d9b0f6bd6ab7721f922a775e4b205fe6f8f6f05
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-20 12:33:34 +0300

    std lib work

---
+++ Diena_11_modules_packages_std/using_standard_lib.py
@@ -0,0 +1,15 @@
+# using Python standard library
+# https://docs.python.org/3/library/
+# Extensive - Batteries Included - just import!
+# Available across most OSes
+
+import string
+import math
+from math import floor
+from collections import Counter
+
+print(string.ascii_lowercase)
+print(math.factorial(5))
+print(floor(3.98))
+my_count = Counter("abracadabra")
+print(my_count.most_common())
