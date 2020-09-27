c9b4041e9ae4b85874ba34c87f9e8fda627f218b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-09-27 12:24:45 +0300

    Add Matplotlib examples

--- Diena_13_Visualization/matplot.py
+++ Diena_13_Visualization/matplot.py
@@ -1,11 +1,18 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-
-print("Making some images")
+import matplotlib.pyplot as plt  # very typical
+# Mathplotlib library
+# inspired by MATLAB
+# https://matplotlib.org/3.3.2/contents.html
+# https://realpython.com/python-matplotlib-guide/
+# https://matplotlib.org/3.3.2/tutorials/index.html
+# print("Making some images")
 fig = plt.figure()
 
-x = list(range(10))
+x = list(range(11))
 y = [n**2 for n in x]
+y2 = [n+5 for n in y]
+y3 = [n+15 for n in y]
 plt.bar(x, y)
+plt.plot(x, y2)
+plt.scatter(x, y3)
 
 plt.show()
---
+++ using_pip.md
@@ -0,0 +1,9 @@
+# PIP Python Package Manager
+
+### https://pypi.org/
+
+The Python Package Index (PyPI) is a repository of software for the Python programming language.
+
+#### https://docs.python.org/3/installing/index.html
+
+#### https://realpython.com/what-is-pip/
