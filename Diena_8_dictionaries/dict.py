7d085c119f31a4555af946434e21a984e56c4a5b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-10-29 19:01:22 +0200

    dict comprehension

--- Diena_8_dictionaries/dict.py
+++ Diena_8_dictionaries/dict.py
@@ -1,44 +1,53 @@
-# # Python Dictionaries
-# # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
-# # key - value pairs
-# # unordered
-# # other languages - associative array, map, hashmap
-# # O(1) value lookup - very quick
+# # # Python Dictionaries
+# # # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+# # # key - value pairs
+# # # unordered
+# # # other languages - associative array, map, hashmap
+# # # O(1) value lookup - very quick
 empty_dict = {} # 
 print(len(empty_dict))
 print(type(empty_dict))
 tel = {'valdis': 2640, 'līga': 2911}
 print(tel)
 tel['maija'] = 2653
-print(tel)
-print(tel['valdis']) # this lookup will be very fast O(1) even with huge dictionary
-key = "maija"
-print(tel[key])
-may_phone = tel[key]
-print(may_phone)
-# # keys are unique, values can match for different keys
+# print(tel)
+# print(tel['valdis']) # this lookup will be very fast O(1) even with huge dictionary
+# key = "maija"
+# print(tel[key])
+# may_phone = tel[key]
+# print(may_phone)
+# # # keys are unique, values can match for different keys
+# tel['valdis'] = 2900
+# print(tel)
+# tel['valdis'] = [2900, 2911, 2640]
+# print(tel)
+# tel['valdis'] = 2640
+# print(tel)
 tel['petēris'] = 2911
 print(tel)
-print(tel.keys(), type(tel.keys()))
-key_list = list(tel.keys()) # so i can create a list of dictionary keys
-print(key_list)
-value_list = list(tel.values())
-print(value_list)
-print('valdis' in tel) # so i check for key in my dictionary
-print('joker' in tel)
-# print(tel['badkey'])
-print(2640 in tel.values()) # just in keep in mind this will be slow on big dictionaries
-my_key = "petēris"
-# my_key = "badpetēris"
-if my_key in tel:
-    print(tel[my_key])
-else:
-    print("Couldn't find this key")
-print(tel.get('valdis'))
-print(tel.get('nevaldis'))  # default is None if not found
-print(tel.get('nevaldis', "couldn't find the key"))
+# print(tel.keys(), type(tel.keys()))
+# key_list = list(tel.keys()) # so i can create a list of dictionary keys
 
-# # iterate over all items (key->value pairs) in dictionary
+# print(key_list)
+# value_list = list(tel.values())
+# print(value_list)
+# print('valdis' in tel) # so i check for key in my dictionary
+# print('joker' in tel)
+
+# print(2640 in tel.values()) # just in keep in mind this will be slow on big dictionaries
+
+# # print(tel['badkey'])
+# my_key = "petēris"
+# my_key = "badpetēris"
+# if my_key in tel:
+#     print(tel[my_key])
+# else:
+#     print("Couldn't find this key")
+# print(tel.get('valdis')) # so we get value by key 'valdis' similar to tel['valdis'] but without errors
+# print(tel.get('nevaldis'))  # default is None if not found
+# print(tel.get('nevaldis', "couldn't find the key"))
+
+# # # iterate over all items (key->value pairs) in dictionary
 for key, value in tel.items():
     # it is important not to modify this dictionary while we are looping over it
     print(f"{key=}, {value=}")
@@ -47,9 +56,11 @@ for key, value in tel.items():
 
 # idea to set key value pair UNLESS it is already set
 tel.setdefault("rūta", 2911)
-
-tel["valdis"] = 2911 # so changing value
+tel.setdefault("rūta", 5555) # so this will not run because "rūta" is already set
 print(tel)
+
+# tel["valdis"] = 2911 # so changing value
+# print(tel)
 
 value_to_find = 2911
 new_dict = {}
@@ -62,9 +73,9 @@ print(new_dict)
 del tel['petēris']
 print(tel)
 
-# # set value for key if not set
-# tel.setdefault('pēteris', 2911)
-# # the above will not overwrite unlike tel['pēteris'] = 2911
+# # # set value for key if not set
+# # tel.setdefault('pēteris', 2911)
+# # # the above will not overwrite unlike tel['pēteris'] = 2911
 
 # complete clearing of dictionary
 tel.clear()
--- Diena_8_dictionaries/dict_comprehension.py
+++ Diena_8_dictionaries/dict_comprehension.py
@@ -5,7 +5,7 @@
 # my_squares = {str(n): n*n for n in range(5)} # dictionary comprehension
 # print(my_squares)
 
-
+# # so same as doing the following
 # squares = {}
 # for n in range(5):
 #     squares[str(n)] = n*n
@@ -16,10 +16,18 @@
 
 # my_cubes = {f"Cube of {n}": n**3 for n in range(8)}
 # print(my_cubes)
-# food = "kartupelis"
-# abc = "abcdefghij"
-# cypher = {k: v for k, v in zip(food, abc)}
-# print(cypher)
+food = "kartupelis"
+abc = "abcdefghij" # so both strings of same length
+cypher = {k: v for k, v in zip(food, abc)}
+print(cypher)
+
+def ceasar(plain_text, cypher=cypher):
+    secret_list = [cypher.get(c, "X") for c in plain_text] # so replace each letter with letter from dictionary cypher
+    return "".join(secret_list)
+
+print(ceasar("art"))
+print(ceasar("tupi"))
+print(ceasar("valdis"))
 # abc_dict = {v: i for i, v in enumerate(abc, start=1)}
 # print(abc_dict)
 # import string
@@ -27,14 +35,25 @@
 # print(all_letter_dictionary)
 # zero_dict = {c: 0 for c in food}
 # print(zero_dict)
-tel = {'valdis': 2640, 'līga': 2911, "maija":2911}
+tel = {'valdis': 2640, 'līga': 2911, "maija":2911, "ede": 2911, "pēteris":2644}
+
 
 def filter_dictionary(in_dict, value_to_find=2911):
-    out_dict = {k:v for k,v in in_dict.items() if v == value_to_find}
+    out_dict = {key:value for key,value in in_dict.items() if value == value_to_find}
     return out_dict
+
+def filter_dict_without_comprehension(in_dict, value_to_find=2911):
+    out_dict = {}
+    for key,value in in_dict.items():
+        if value == value_to_find:
+            out_dict[key] = value # or out_dict.setdefault(key, value)
+    return out_dict
+
 
 my_filtered_dict = filter_dictionary(tel)
 print(my_filtered_dict)
+my_filtered_dict2 = filter_dict_without_comprehension(tel)
+print(my_filtered_dict2)
 new_dict = filter_dictionary(tel, 2640)
 print(new_dict)
 
