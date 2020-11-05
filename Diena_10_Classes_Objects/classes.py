615a08b31ecc0cd17150cf37e6e541110decfc21
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-05 20:51:26 +0200

    Class Rap Song

--- Diena_10_Classes_Objects/classes.py
+++ Diena_10_Classes_Objects/classes.py
@@ -1,23 +1,23 @@
-# # Python Class
-# # Classes -> combine related data(properties)
-# # Classes -> combine related funcionality(methods)
-# # Objects - instances of class
-# # Classes: blueprints
-# # Objects: concrete realization of those blueprints
-# # https://docs.python.org/3/tutorial/classes.html
+# # # Python Class
+# # # Classes -> combine related data(properties)
+# # # Classes -> combine related funcionality(methods)
+# # # Objects - instances of class
+# # # Classes: blueprints
+# # # Objects: concrete realization of those blueprints
+# # # https://docs.python.org/3/tutorial/classes.html
 
-# # how to store and process data about garage
+# # # how to store and process data about garage
 my_garage = ["paint can", "old papers", "rotting potatoes"]
 my_garage_d = {"paints": ["white", "black"], "food": "potatoes"}
 
-# # empty class definition
+# # # empty class definition
 
 
 # class EmptyClass:
 #     pass
 
 
-# # # i create an  based on class blueprint
+# # # # i create an  based on class blueprint
 
 # empty_class_instance_object = EmptyClass()
 # empty_class_instance_object.paint = ["red", "blue"]
@@ -29,66 +29,65 @@ my_garage_d = {"paints": ["white", "black"], "food": "potatoes"}
 # empty_2.paint = "Guasha"
 # print(empty_2.paint, empty_class_instance_object.paint)
 
-# # the simplest empty class definition
+# # # the simplest empty class definition
 
-# color = "Global color"
-
-
-class House:
-    def __init__(self, color="green", nails=0):
-        self.color = color
-        self.nails = nails
-        print(f"Initialized class instance with {self.color=} {self.nails=}")
-
-    def simple_print(self):
-        print(f"Oh {self.color=} {self.nails=}")
-        return self
-
-    def set_color(self, new_color):
-        # some verification on sane color here maybe
-        self.color = new_color
-        print(f"Changed color to {self.color}")
+# # color = "Global color"
 
 
-# my_house = House() # creaing new object, in other class instance
-# i've created an object my_house based on House class blueprints
-# print(type(my_house))
-# my_house.simple_print()
+# class House:
+#     def __init__(self, color="green", nails=0):
+#         self.color = color
+#         self.nails = nails
+#         print(f"Initialized class instance with {self.color=} {self.nails=}")
 
-# friends_house = House(color="blue", nails=1_000)
-# friends_house.simple_print()
-# print(my_house.color)
-# my_house.set_color("red")
-# my_house.simple_print()
+#     def simple_print(self):
+#         print(f"Oh {self.color=} {self.nails=}")
+#         return self
 
-# def inside class defines method (so function which is called by class or object)
+#     def set_color(self, new_color):
+#         # some verification on sane color here maybe
+#         self.color = new_color
+#         print(f"Changed color to {self.color}")
+
+
+# # my_house = House() # creaing new object, in other class instance
+# # i've created an object my_house based on House class blueprints
+# # print(type(my_house))
+# # my_house.simple_print()
+
+# # friends_house = House(color="blue", nails=1_000)
+# # friends_house.simple_print()
+# # print(my_house.color)
+# # my_house.set_color("red")
+# # my_house.simple_print()
+
+# # def inside class defines method (so function which is called by class or object)
 
 
 class Garage:
-    g_name = "just a garage" # better to sparingly we can run in some weird effects
-
-    # classes constructor method called when we make a new object instance from this class
-    # dunder syntax __init__
-    def __init__(self, color="green", nails=0):
+    # g_name = "just a garage" # better to sparingly we can run in some weird effects
+    # _nail_color = "metalic" # by convention private
+#     # classes constructor method called when we make a new object instance from this class
+#     # dunder syntax __init__
+    def __init__(self, color="green", nails=0, name="My garage", nail_color="metal"):
         self.color = color
         self.nails = nails
-        print(f"Initialized class instance with {self.color=} {self.nails=}")
+        self.name = name
+        self._nail_color = nail_color # so called private variable
+        self.__secret_stash = "gold"
+        print(f"Initialized class instance with {self.color=} {self.nails=} {self.name=}")
 
-    def __str__(self): # this is responsible for string representation
-        return f"My garage {self.color} with nails: {self.nails} and name: {self.g_name}"
+    def __str__(self): # this is responsible for string representation for print etc
+        return f"My garage {self.color} with nails: {self.nails} and name: {self.name}"
 
-    def __repr__(self):
-        return f"My garage {self.color} and name: {self.g_name}"
-
-    def __add__(self, other):
+    def __add__(self, other): # + operator overloading in other languages
         # return self.nails + other.nails
         new_color = self.color+"_"+other.color
         new_nails = self.nails + other.nails
-        return Garage(new_color, new_nails)
+        return Garage(new_color, new_nails) # i create new instance  with new attributes
 
-
-    def simple_print(self):
-        print(f"Oh {self.g_name=} {self.color=} {self.nails=}")
+    def simple_print(self): # so this name i just made up myself
+        print(f"Oh {self.name=} {self.color=} {self.nails=}")
         return self
 
     def add_nails(self, new_nails=1):
@@ -99,64 +98,83 @@ class Garage:
         self.nails = new_nail_count
         return self
 
-    # OOP getters method
+#     # OOP getters method
     def get_current_nails(self):
         # formatting, data sanitation, so on
         return self.nails+1000
 
+simple_garage = Garage()
+simple_garage.simple_print()
+simple_garage.add_nails(15)
+simple_garage.simple_print()
 
-# create new objects based on class definition
+# print(simple_garage.g_name)
+# simple_garage.g_name = "Mana garāža"
+# print(simple_garage.g_name)
+# to avoid always initalize by hand constructors were created
+
+# # create new objects based on class definition
 homer_garage = Garage(nails=33)
-flanders_garage = Garage(nails=55)
-# two different objects from the same blueprint(class defintion)
+flanders_garage = Garage(color="blue", nails=55, name="Property of Flanders")
+mutant_garage = homer_garage + flanders_garage
+mut_garage = homer_garage.__add__(flanders_garage) # same as above
+# garage_obj_1 = Garage()
+# garage_obj_2 = Garage()
+# print(garage_obj_1 == garage_obj_2) # to compare we'd need to define our __eq__ method
+# print(garage_obj_1 is garage_obj_2) # main thing that garage object reside in diffent memory location
+# # two different objects from the same blueprint(class defintion)
 print(id(homer_garage), id(flanders_garage))
-print(homer_garage.g_name)
-print(Garage.g_name) 
-# homer_garage.simple_print()
-# flanders_garage.simple_print()
-millhouse_garage = Garage(color="purple")
-millhouse_garage.simple_print()
-
-homer_garage.g_name = "Homer's garage"
-homer_garage.simple_print()
-print(Garage.g_name) 
 print(homer_garage)
-super_garage = homer_garage + flanders_garage
-print(super_garage)
-# simpsons_house = House(color="yellow")
-# print(simpsons_house.color)
-# simpsons_house.simple_print()
-# print(str(simpsons_house))
-# print(homer_garage.nails)
-# print(homer_garage.get_current_nails())
-# print(homer_garage.nails)
-
-# millhouse_garage.nails
-# millhouse_garage.add_nails(7)
-# millhouse_garage.add_nails(17)
+print(homer_garage._nail_color)
+homer_garage.add_nails(50).add_nails(170).simple_print() # so return self lets me chain methods
+# print(homer_garage.__secret_stash) # so __variables get name mangled
+# print(homer_garage.g_name)
+# print(Garage.g_name) 
+# # homer_garage.simple_print()
+# # flanders_garage.simple_print()
+# millhouse_garage = Garage(color="purple")
 # millhouse_garage.simple_print()
-# millhouse_garage.add_nails(10).add_nails(25).simple_print()
-# millhouse_garage.add_nails(5).simple_print()
-# millhouse_garage.set_nails(-50).add_nails(10).simple_print()
 
-# # # homer_garage.paints = ["white", "black"]
-# # # homer_garage.foods = "eaten"
-# # # print(homer_garage.paints)
-# # # print(homer_garage.foods)
+# homer_garage.g_name = "Homer's garage"
+# homer_garage.simple_print()
+# print(Garage.g_name) 
+# print(homer_garage)
+# super_garage = homer_garage + flanders_garage
+# print(super_garage)
+# # simpsons_house = House(color="yellow")
+# # print(simpsons_house.color)
+# # simpsons_house.simple_print()
+# # print(str(simpsons_house))
+# # print(homer_garage.nails)
+# # print(homer_garage.get_current_nails())
+# # print(homer_garage.nails)
 
-# # # FancyGarage will inherit everything from Garage
+# # millhouse_garage.nails
+# # millhouse_garage.add_nails(7)
+# # millhouse_garage.add_nails(17)
+# # millhouse_garage.simple_print()
+# # millhouse_garage.add_nails(10).add_nails(25).simple_print()
+# # millhouse_garage.add_nails(5).simple_print()
+# # millhouse_garage.set_nails(-50).add_nails(10).simple_print()
 
-# inheritance
+# # # # homer_garage.paints = ["white", "black"]
+# # # # homer_garage.foods = "eaten"
+# # # # print(homer_garage.paints)
+# # # # print(homer_garage.foods)
+
+# # # # FancyGarage will inherit everything from Garage
+
+# # inheritance
 class FancyGarage(Garage): # so I say that this class blueprints use all methods and attributes from Garage class
     # Class Attribute
     gtype = "Fancy"
     # constructor method, called when we created object from this class
     total_travel = 0
 
-    def __init__(self, cars, wines, color="Gold"):
+    def __init__(self, cars, wines, color="Gold",name="Garage"):
         # I call my parent class constructor
         # Python 3.x+ we call our parent class constructor
-        super().__init__(color, nails=2000)
+        super().__init__(color, nails=2000, name=name)
         # self.color = color  # for color this would work, but we would have no nails!!
         self.cars = cars
         self.wines = wines
@@ -178,36 +196,36 @@ class FancyGarage(Garage): # so I say that this class blueprints use all methods
         return wines_length[0]
 
 
-burns_garage = FancyGarage("Bentley", ["Rioja", "Temparillo", "Riesling"])
-crusty_garage = FancyGarage("Rolls", ["Cabernet", "Cheap Wine"], "Bright Red")
+burns_garage = FancyGarage("Bentley", ["Rioja", "Temparillo", "Riesling"], name="Mr. Burn's Garage")
+crusty_garage = FancyGarage("Rolls", ["Cabernet", "Cheap Wine"], "Bright Red", name="Crusty's Garage")
 
-# burns_garage.pretty_print()
-burns_garage.add_nails(10).drive(20).drive(30).pretty_print().simple_print()
-burns_garage.add_nails(50).drive(70).pretty_print()
-# print(burns_garage.get_longest_wine())
-
-# kent_garage = FancyGarage("Ferrari", wines="Cheap Wine",color="aquamarine")
-# kent_garage.pretty_print()
-
-# # print(burns_garage.cars)
-# # print(burns_garage.wines)
-# # burns_garage.gtype = "Rich"
 # # burns_garage.pretty_print()
-# # crusty_garage.pretty_print()
+burns_garage.add_nails(10).drive(20).drive(30).pretty_print().simple_print()
+burns_garage.add_nails(50).drive(70).pretty_print().simple_print()
+# # print(burns_garage.get_longest_wine())
 
+# # kent_garage = FancyGarage("Ferrari", wines="Cheap Wine",color="aquamarine")
+# # kent_garage.pretty_print()
 
-# # # crusty_garage.drive(60)
-# # # crusty_garage.drive(160)
-# # # crusty_garage.drive(20)
+# # # print(burns_garage.cars)
+# # # print(burns_garage.wines)
+# # # burns_garage.gtype = "Rich"
+# # # burns_garage.pretty_print()
 # # # crusty_garage.pretty_print()
-# # # print(burns_garage.get_longest_wine())
-# # # print(crusty_garage.get_longest_wine())
-# # burns_garage.drive(100).drive(150).drive(80).pretty_print()
-# # crusty_garage.drive(10).drive(25).pretty_print()
-# # wilma_garage = Garage("pink")
-# # wilma_garage.simple_print()
-# # burns_garage.simple_print()
-# # brockman_garage = FancyGarage("Ferrari", "Chuck Wine")
-# # comic_guy_garage = FancyGarage("Ferrari", "Chuck Wine", "Bright Red")
-# # comic_guy_garage.simple_print()  # can use Garage method
-# # comic_guy_garage.pretty_print()  # can use Fancy Garage method
+
+
+# # # # crusty_garage.drive(60)
+# # # # crusty_garage.drive(160)
+# # # # crusty_garage.drive(20)
+# # # # crusty_garage.pretty_print()
+# # # # print(burns_garage.get_longest_wine())
+# # # # print(crusty_garage.get_longest_wine())
+# # # burns_garage.drive(100).drive(150).drive(80).pretty_print()
+# # # crusty_garage.drive(10).drive(25).pretty_print()
+# # # wilma_garage = Garage("pink")
+# # # wilma_garage.simple_print()
+# # # burns_garage.simple_print()
+# # # brockman_garage = FancyGarage("Ferrari", "Chuck Wine")
+# # # comic_guy_garage = FancyGarage("Ferrari", "Chuck Wine", "Bright Red")
+# # # comic_guy_garage.simple_print()  # can use Garage method
+# # # comic_guy_garage.pretty_print()  # can use Fancy Garage method
---
+++ Diena_10_Classes_Objects/u1_g2.py
@@ -0,0 +1,43 @@
+class Song():
+    def __init__(self, title="", author="", lyrics=()):
+        self.title = title
+        self.author = author
+        self.lyrics = lyrics
+        print(f"New Song made: {self.author=}, {self.title=}")
+ 
+    def sing(self, max_lines=-1, sing_loud=False):
+        self.print_credentials()
+        for i, line in enumerate(self.lyrics):
+            if i == max_lines: # so -1 will be never be reached and ALL lines will be printed
+                break
+            print(line.upper() if sing_loud else line)
+        return self
+ 
+    def yell(self, max_lines=-1):
+        self.sing(max_lines=max_lines, sing_loud=True)
+        return self
+ 
+    def print_credentials(self):
+        if self.title != "" and self.author != "":
+            print(f"{self.title} - {self.author}")
+        return self
+ 
+class Rap(Song):
+    def __init__(self, title="", author="", lyrics=()):
+        super().__init__(title=title, author=author, lyrics=lyrics)
+ 
+    def break_it(self, max_lines=-1, drop="yeah"):
+        self.print_credentials()
+        for i, line in enumerate(self.lyrics):
+            if max_lines == i:
+                break
+            temp_list = line.split(" ")
+            drop_to_add = f" {drop.upper()} "
+            print(drop_to_add.join(temp_list) + drop_to_add[:-1])
+        return self
+    
+ziemelmeita = Song("Ziemeļmeita", "Jumprava", ["Gāju meklēt ziemeļmeitu","Garu, tālu ceļu veicu"])
+ziemelmeita.sing(1).yell(1)
+ 
+zrap = Rap("Ziemeļmeita", "Jumprava", ["Gāju meklēt ziemeļmeitu","Garu, tālu ceļu veicu"])
+zrap.break_it(1, "yah").sing().yell(2).break_it(drop="aha")
