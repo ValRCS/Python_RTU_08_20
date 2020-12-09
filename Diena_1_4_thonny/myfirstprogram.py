88bb8dc15bdeee445e4ca8528407c4f2a17c4579
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-09 19:56:49 +0200

    Changes to dec9 doodle

--- Diena_1_4_thonny/myfirstprogram.py
+++ Diena_1_4_thonny/myfirstprogram.py
@@ -1,17 +1,22 @@
-print("Hello world!")
+print("Hello world!") # print ("not good style")
 # print("Hello again!")
 # print('Single quotes work as well')
 # print(5+5)
 # this is a comment for humans not computer
 # print("Valdis" + 5) # this will not work
-print("this will not plint but print")
+print("   this will not plint but print")
 print("this will not end well ")
-print("alus " * 7) # one of the little Python abilities
+print("alus "*7) # one of the little Python abilities
 print(100*2*5-50)
 print(1_000_000+9_000)
 print(555/111)
 print(500/55)
-print(10**4)
-print(10%7)
+print(10**4) # powers  of 4
+print(10%7) # reminder, important in programming 
+print("Valdis",'likes', "coding")  # with , we get whitespace separator
+print(f"My math: 2*2 = {2*2}")
+print("Valdis " + "food" + " Potatoes")
+print("Valdis food Potatoes")
+
 for n in range(5):
     print("Hello",n)
