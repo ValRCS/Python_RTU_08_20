b6d15e54a25daaccf40fdd9727b4dfd0c7cd2b88
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-01-19 19:53:30 +0200

    reading and writing files

--- Diena_12_Faili/using_csv.py
+++ Diena_12_Faili/using_csv.py
@@ -1,13 +1,20 @@
 import csv
 
-with open("Diena_12_faili/test.csv", encoding="utf-8") as f:
+with open("test.csv", encoding="utf-8") as f:
     my_data = [line.split(",") for line in f]
 print(my_data)
 
-with open("Diena_12_faili/test.csv", encoding="utf-8") as f:
+with open("test.csv", encoding="utf-8") as f:
     spamreader = csv.reader(f, delimiter=',', quotechar='"')
     my_csv_data = [row for row in spamreader]
     print(my_csv_data)
+
+my_csv_data.append([5324,254524,24352])
+
+with open("test-add.csv", mode="w",encoding="utf-8") as f:
+    spamwriter = csv.writer(f, delimiter=';',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerows(my_csv_data)
 
 # with open('eggs.csv', 'w', newline='') as csvfile:
 #     spamwriter = csv.writer(csvfile, delimiter=' ',
