7e765c9ae58e7c309661a1ba8a7fa0b5148430c3
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-03 18:53:04 +0200

    Day 9 Group 3

---
+++ Diena_1_4_thonny/hello_world.py
@@ -0,0 +1,21 @@
+for x in range(5):
+    print("Valdis")
+print("alus "*5)
+# print("Valdis")
+# print("Valdis")
+# print("Valdis")
+# print("Valdis")
+
+# print("Hello World!")
+# print("Computer do something....")
+# print(2+2)
+# print(2*3)
+print(10/5) # regular division
+# print(10//5)
+# print(9//4) # dividing without reminder just the whole
+# print(2**8) # power
+# print(2+2*5)
+
+print(365*24*3600)
+print("")
+print (10**1000)
