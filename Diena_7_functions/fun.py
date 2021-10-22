c26bdde7b0b2fac428468bdb4f1b47fc82287434
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-10-22 18:56:04 +0300

    add fun changes

--- Diena_7_functions/fun.py
+++ Diena_7_functions/fun.py
@@ -1,54 +1,56 @@
-# # # # # # # # # # # formal https://docs.python.org/3/tutorial/controlflow.html#defining-functions
-# # # # # # # # # # # built in functions https://docs.python.org/3/library/functions.html
-# # # # # # # # # # # DRY ! https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
+# # # # # # # # # # # # formal https://docs.python.org/3/tutorial/controlflow.html#defining-functions
+# # # # # # # # # # # # built in functions https://docs.python.org/3/library/functions.html
+# # # # # # # # # # # # DRY ! https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
 
-# # # # # # # # # # # we can give our functions parameters and those parameters take arguments
+# # # # # # # # # # # # we can give our functions parameters and those parameters take arguments
 
 # print("Go eat")
 # print("Let's order food")
 
 # print("maybe i do something else")
-# # lots of code could happen here
+# # # # lots of code could happen here
 # print("Go eat")
 # print("Let's order food")
 # print("Go eat")
 # print("Let's order food")
 
-# # # # # # # # defining simple function
+# go_eat() # not yet known
+# # # # # # # # # defining simple function
 def go_eat():
     # function block starts here
     print("Go eat fast food")
     print("Let's order food RIGHT Now!")
+    print("Toast the host!")
     print("Pay and Leave")
     # we could add more stuff for our function here include loops, ifs etc
 
-# # # # # # # # # # # calling function
+# # # # # # # # # # # # calling function
 # go_eat()
 # go_eat()
-# # # # # print("Do something else")
+# # # # # # # print("Do something else")
 # for _ in range(4): # if we do not need the loop variable we use _ 
 #     go_eat()
-# # # # go_eat()
+# go_eat()
 
-# # # # # # # # # # # requirement that order_food is given an argument
+# # # # # # # # # # # # requirement that order_food is given an argument
 def order_food(dish):
     dish = str(dish) # because we will be using capitalize later on 
     print(f"I am ordering {dish}")
     print(f"{dish.capitalize()} should be pretty tasty")
     # so dish stops working here
-# # # # # # # no dish here
+# # # # # # # # no dish here
 
 
 # order_food("potatoes")
 # order_food("ice cream")
-# my_soup = "Beet soup"
-# order_food(my_soup)
+my_soup = "Beet soup"
+# order_food(my_soup) # i can pass a variable as an argument
 # order_food(555)
 
 # print(dish) # dish is not available globally because it is only inside function
 
 
-# # # # # # # # # # # # go to restaurant
+# # # # # # # # # # # # # go to restaurant
 
 
 def eat(food_list):
@@ -62,20 +64,21 @@ def eat(food_list):
     print("Let's leave and be happy")
 
 
-# food_list = ["soup", "potatoes", "ice cream"]
+food_list = ["soup", "potatoes", "ice cream"]
 # eat(food_list)
-# another_food_list = ["cabbage", "beans", "onions"]
-# eat(another_food_list)
+# print("Whew I am full")
+# # another_food_list = ["cabbage", "beans", "onions"]
+# # eat(another_food_list)
 
-# # # # # # # # # # # # call the function 2 times
-# # # # # # eat(["soup", "potatoes", "ice cream"])
-# # # # # # eat(["Cabbage"]) # i need one argument for eat function
-# # eat(["Kartupelis"]) # so we passed a list of one item
-# # # # # # # what happens if we pass our function a string...
-# # eat("Kartupelis") # turns out we will go through our food one letter at a time...
-# # eat(55) # we cant go through numbers
+# # # # # # # # # # # # # call the function 2 times
+# # # # # # # eat(["soup", "potatoes", "ice cream"])
+# # # # # # # eat(["Cabbage"]) # i need one argument for eat function
+# # # eat(["Kartupelis"]) # so we passed a list of one item
+# # # # # # # # what happens if we pass our function a string...
+# # # eat("Kartupelis") # turns out we will go through our food one letter at a time...
+# # # eat(55) # we cant go through numbers
 
-# # i can add another parameter
+# # # i can add another parameter
 def add(a,b):
     print(f"{a}+{b}={a+b}")
     # so by default we return None !
@@ -91,7 +94,7 @@ result = 55 # this is global not used inside mult!
 def mult(a,b):
     result = a*b # result could be another name, this is a local variable not used outside function
     print(f"{a}*{b}={result}") # print is not always needed in a function, print is heavy
-    return result # we can return anything
+    return result # we can return anything, local function result ends its life here
 
 my_calc = mult(5,8)
 print(my_calc, result) # so result is still 55
@@ -101,17 +104,18 @@ print(result)
 multi_calc_result = mult(mult(5,10), mult(10,2)) # with return I gain ability to apply result to next function
 print(multi_calc_result)
 
+# i can return multiple values at once
 def multi_calc(a,b):
     # could add check for 0
-    return a*b, int(a/b) # could return even more, multiple values are returned as tuples
+    return a*b, int(a/b) # could return even more with comma multiple values are returned as tuples
 
 my_mult, my_div = multi_calc(10,2) # so called tuple unpacking 
 print(my_mult,my_div)
-# # print(multi_calc(9,2)) # parenthesis show up because we are returning a tuple(which is likea  fixed list)
+# # # print(multi_calc(9,2)) # parenthesis show up because we are returning a tuple(which is likea  fixed list)
 
 
 # Default argument idea
-def big_calc(a=-500,b=1000,c=100, verbose=False, new_option="Badac"): # so c will be 100 if not given as argument
+def big_calc(a=-500,b=1000,c=100, verbose=False): # so c will be 100 if not given as argument
     result = a*b+c
     if verbose:
         print(f"Incoming Arguments: a {a}, b {b}, c {c}")
@@ -124,11 +128,11 @@ my_big = big_calc(3,4,10) # so we can use it normally
 print(my_big)
 another_calc = big_calc(3,5) # notice we only need 2 arguments, and c will be default 100
 print(another_calc)
-# print(my_big, another_calc)
+# # print(my_big, another_calc)
 
-# print(big_calc(5)) # only giving value for a, : b,c use defaults (1000 and 100)
+print(big_calc(5)) # only giving value for a, : b,c use defaults (1000 and 100)
 print(big_calc()) # this will use ALL 3 default values
-# print(big_calc(c = 10, b = 5, a = 4)) # i can pass arguments in whichever order I want if i use named arguments
+print(big_calc(c = 10, b = 5, a = 4)) # i can pass arguments in whichever order I want if i use named arguments
 print(big_calc(b=25)) # i can pass only one named argument, the rest stay default
 
 print(big_calc(10,30,50, verbose=True))
@@ -152,28 +156,28 @@ talk("Hello there", is_yelling=True) # this is more clear
 # print(talk("Hello", is_yelling=True).lower()) # if function returns something i can chain it
 # # # # talk("Aha", True)
 
-# # # # # add(b=10, a=3)
+# # # # # # add(b=10, a=3)
 
-# print("Valdis", "coding", end="\n\n\n!*-*\n\n", sep="|||") # a bit extreme for end and sep but your choice
+print("Valdis", "coding", end="\n\n\n!*-*\n\n", sep="|||") # a bit extreme for end and sep but your choice
 
-# # # # # # so in Python data types in function parameters are just hints! they do not affect function functionality
+# # # # # # # so in Python data types in function parameters are just hints! they do not affect function functionality
 def diff(a: int, b: int) -> int: # we defined a function signature , what data types should be used
     result = round(a-b,2)
     print(f"{a}-{b}={result}")
     return a-b
     # return "badac"
 
-diff(10,3)
-diff(10.6,3.7) # even if the data types are wrong, Python will still run the program
+print(diff(10,3))
+# print(diff(10.6,3.7)) # even if the data types are wrong, Python will still run the program
 
 def get_string_length(text: str) -> int:
     return len(text)
 
 print(get_string_length("Valdis"))
-print(get_string_length([1,2,6]))
-# # # diff("Valdis",35354)
-# # # # it is only static type checking tool such as Pyright extension that will yell at us
+# print(get_string_length([1,2,6]))
+# # # # diff("Valdis",35354)
+# # # # # it is only static type checking tool such as Pyright extension that will yell at us
 
-# # go_eat()
+# # # go_eat()
 
 
--- Diena_7_functions/fun_args.py
+++ Diena_7_functions/fun_args.py
@@ -1,18 +1,18 @@
-# # https://docs.python.org/3/tutorial/controlflow.html#keyword-arguments
-# # https://realpython.com/python-kwargs-and-args/
+# # # https://docs.python.org/3/tutorial/controlflow.html#keyword-arguments
+# # # https://realpython.com/python-kwargs-and-args/
 
-# print(min(1,2,-6,6,20,30))
+print(min(1,2,-6,6,20,30))
 
 def adder(*args): # so a list of arguments of any length
     print(f"My adder with {len(args)} arguments, args is {type(args)}")
     for arg in args:
         print(arg)
 
-# adder() # will work even with no arguments
-# adder(5, 2, "Valdis")
-# adder(5, 2, "Valdis", "Līga", 1222)
+adder() # will work even with no arguments
+adder(5, 2, "Valdis")
+adder(5, 2, "Valdis", "Līga", 1222)
 
-def summator(*args, verbose=False):
+def summator(*args, verbose=False):  # so verbose is a named argument
     if verbose:
         print(f"got {args}")
     result = sum(args)
@@ -20,31 +20,43 @@ def summator(*args, verbose=False):
         print("result is", result)
     return result
 
-my_result = summator(23,3,234,2235)
+my_result = summator(23,3,234,2235)  # notice this is not a list
 print(my_result)
 another_result = summator(5,2,6,20, verbose=True)
 print(another_result)
-# # summator(23,3,234,2235,1000)
-# # summator()
+another_sum = summator(23,3,234,2235,1000)
+print(another_sum)
 
 
-def mult(*args, multiplier=1): #so positional arguments have to come first, named afterwards
-    result = multiplier
-    for arg in args:
-        # could add an if here to check data type if we are not sure of numbers
-        result *= arg
-    return result 
+# def mult(*args, multiplier=1): #so positional arguments have to come first, named afterwards
+#     result = multiplier
+#     for arg in args:
+#         # could add an if here to check data type if we are not sure of numbers
+#         result *= arg
+#     return result 
     
     
-print("Checking Mult")
-print(mult())
-print(mult(2, 10, 5))
-print(mult(2, 10, 5, -3.6))
-print(mult(2, 10, 5, -3.6, multiplier=1000))
+# print("Checking Mult")
+# print(mult())
+# print(mult(2, 10, 5))
+# print(mult(2, 10, 5, -3.6))
+# print(mult(2, 10, 5, -3.6, multiplier=1000))
 
-print(mult(2, 10, 5, -3.6, multiplier=0))
+# # print(mult(2, 10, 5, -3.6, multiplier=0))
 
 
-# # print("A", 5, mult(3, 6))
+# # # print("A", 5, mult(3, 6))
 
-# # TODO iespēja padot arī **kwargs after we learn about dictionaries
+# # # TODO iespēja padot arī **kwargs after we learn about dictionaries
+
+def fun(*argv, required, my_default="Given as default"):
+    # so what will be the minimu arguments that I need to add?
+    print(f"got {len(argv)} positional arguments")
+    for arg in argv:
+        print("arg:", arg)
+    print(f"Required arg is {required}")
+    print(f"My default is {my_default}")
+
+fun(required="hmm") # so hmm will be assigned to required
+fun(1,2, "oho", required="hmm") # so hmm will be assigned to required
+fun(1,2, "oho", required="hmm", my_default="changed default") # so hmm will be assigned to required
