31a0ffa43df3576d7e01fefea62d3c838146b0d6
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-21 19:03:03 +0300

    function fun m28

--- Diena_7_functions/fun_args.py
+++ Diena_7_functions/fun_args.py
@@ -2,6 +2,7 @@
 # # # https://realpython.com/python-kwargs-and-args/
 
 print(min(1,2,-6,6,20,30))
+print(max(3,7,2))
 
 def adder(*args): # so a list of arguments of any length
     print(f"My adder with {len(args)} arguments, args is {type(args)}")
@@ -10,7 +11,7 @@ def adder(*args): # so a list of arguments of any length
 
 adder() # will work even with no arguments
 adder(5, 2, "Valdis")
-adder(5, 2, "Valdis", "Līga", 1222)
+adder(5, 2, "Valdis", "Līga", 1222, None, True, 3.15)
 
 def summator(*args, verbose=False):  # so verbose is a named argument
     if verbose:
@@ -24,39 +25,40 @@ my_result = summator(23,3,234,2235)  # notice this is not a list
 print(my_result)
 another_result = summator(5,2,6,20, verbose=True)
 print(another_result)
-another_sum = summator(23,3,234,2235,1000)
-print(another_sum)
+# another_sum = summator(23,3,234,2235,1000)
+# print(another_sum)
 
 
-# def mult(*args, multiplier=1): #so positional arguments have to come first, named afterwards
-#     result = multiplier
-#     for arg in args:
-#         # could add an if here to check data type if we are not sure of numbers
-#         result *= arg
-#     return result 
+def mult(first: int, *args: list[int], multiplier=1): #so positional arguments have to come first, named afterwards
+    result = multiplier * first
+    for arg in args:
+        # could add an if here to check data type if we are not sure of numbers
+        result *= arg
+    return result 
     
     
-# print("Checking Mult")
-# print(mult())
-# print(mult(2, 10, 5))
-# print(mult(2, 10, 5, -3.6))
-# print(mult(2, 10, 5, -3.6, multiplier=1000))
+print("Checking Mult")
+print(mult(56)) # i need to enter at least one argument since first is positional - ie required
+print(mult(2, 10, 5))
+# # print(mult(2, 10, 5, -3.6))
+print(mult(2, 10, 5, -3.6, multiplier=1000))
 
-# # print(mult(2, 10, 5, -3.6, multiplier=0))
+# # # print(mult(2, 10, 5, -3.6, multiplier=0))
+print(mult(35, multiplier=0.5))
 
 
-# # # print("A", 5, mult(3, 6))
+# # # # print("A", 5, mult(3, 6))
 
-# # # TODO iespēja padot arī **kwargs after we learn about dictionaries
+# # # # TODO iespēja padot arī **kwargs after we learn about dictionaries
 
-def fun(*argv, required, my_default="Given as default"):
-    # so what will be the minimu arguments that I need to add?
-    print(f"got {len(argv)} positional arguments")
-    for arg in argv:
-        print("arg:", arg)
-    print(f"Required arg is {required}")
-    print(f"My default is {my_default}")
+# def fun(*argv, required, my_default="Given as default"):
+#     # so what will be the minimu arguments that I need to add?
+#     print(f"got {len(argv)} positional arguments")
+#     for arg in argv:
+#         print("arg:", arg)
+#     print(f"Required arg is {required}")
+#     print(f"My default is {my_default}")
 
-fun(required="hmm") # so hmm will be assigned to required
-fun(1,2, "oho", required="hmm") # so hmm will be assigned to required
-fun(1,2, "oho", required="hmm", my_default="changed default") # so hmm will be assigned to required
+# fun(required="hmm") # so hmm will be assigned to required
+# fun(1,2, "oho", required="hmm") # so hmm will be assigned to required
+# fun(1,2, "oho", required="hmm", my_default="changed default") # so hmm will be assigned to required
