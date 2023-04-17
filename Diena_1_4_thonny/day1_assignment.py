def0fd3767c246119c67281b0af4fa776992427f
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2023-04-17 20:37:52 +0300

    Create day1_assignment.py

---
+++ Diena_1_4_thonny/day1_assignment.py
@@ -0,0 +1,12 @@
+print("Hello there web scraper! 😅")
+# Assignments for Day 1
+
+# 1. Create a variable called `name` and assign it your name.
+
+# 2. Print your name five times
+
+# 3. Multiply your name by a number and print the result
+
+# 4. Calculate how many seconds will be this year and print the result
+
+# 5. Print Bananananana using Ba + na * some number
