60fe56b3480adc1c62da0ea7264b109bcf43dc79
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-04-19 19:56:46 +0300

    loop exercises

---
+++ Web_Scraping/looping_classwork.md
@@ -0,0 +1,43 @@
+## . FizzBuzz
+
+* Izdrukāt virknīti 1,2,3,4,Fizz,6,Buzz,8,.....34,FizzBuzz,36,....līdz  97,Buzz, 99,Fizz 
+
+* Tātad ja skaitlis dalās ar 5 tad Fizz
+
+* Ja dalās ar 7 tad Buzz,
+
+* Ja dalās ar 5 UN 7 tad FizzBuzz
+
+* savādāk pats skaitlis
+
+Piezīme: šads uzdevums kļuva populārs kā pirmais uzdevums, ko uzdot, lai noteiktu vai cilvēks vispār zina par programmēšanu :)
+
+## 2. Eglīte
+
+* Ievadiet eglītes augstumu
+
+* Izdrukājiet eglīti:
+
+Piem. augstums == 3
+
+Izdruka būtu:
+
+  *
+
+ ***
+
+*****
+
+Piezīme: atceramies, ka vairākus simbolus var izdrukāt piemēram šādi: print(" "*10+"*"*6)
+
+
+
+##  Pirmskaitlis
+
+* Atrodiet vai ievadītais veselais pozitīvais skaitlis ir pirmskaitlis.
+
+* Pirmskaitlis ir skaitlis, kas dalās bez atlikuma tikai pats ar sevi un 1.
+
+Piezīme: šo uzdevumu var sākt risināt ļoti vienkārši un varat arī pēc tam optimizēt. 
+
+(kaut vai tas, ka mums nav jāpārbauda dalīšanās ar skaitļiem, kas lielāki par ievadītā skaitļa kvadrātsakni)
--- Web_Scraping/loops.py
+++ Web_Scraping/loops.py
@@ -57,10 +57,10 @@ while i < 20:
     if i > 12:
         # we can use continue to skip the rest of the loop
         print("Skipping the rest of the loop")
-        i = i + 2 # we increment i by 2
+        i += 2 # same as i = i + 2 # we increment i by 2
         continue # skip the rest of the loop go back to the top
     print("This will only run if i is less than 12")
-    i = i + 1 # crucial to avoid infinite loop
+    i += 1 # same as i = i + 1 # crucial to avoid infinite loop
 
 
     # we can combine while with try except to process user input
@@ -69,17 +69,116 @@ while i < 20:
     # we will use while True loop
     # we will use try except to catch ValueError
 
-while True: # we could use any condition here for example counter < 3
-    try:
-        text = input("Please enter a whole/integer number ") # no error here
-        number = int(text) # possible ValueError here
-        print(f"You entered {number}")
-        break # we will break out of the loop if we got a number
-    except ValueError:
-        print(f"That {text} was not a whole number")
-        print("Please try again")
-        # if we used a counter here we would increment it here
+# while True: # we could use any condition here for example counter < 3
+#     try:
+#         text = input("Please enter a whole/integer number ") # no error here
+#         number = int(text) # possible ValueError here
+#         print(f"You entered {number}")
+#         break # we will break out of the loop if we got a number
+#     except ValueError:
+#         print(f"That {text} was not a whole number")
+#         print("Please try again")
+#         # if we used a counter here we would increment it here
 
-# we can only arrive here if we got a number
-print("We got a whole number")
-print(f"number is {number}")
+# # we can only arrive here if we got a number
+# print("We got a whole number")
+# print(f"number is {number}")
+
+# philosophically while loop is for indefinite number of iterations
+# we use while when we do not know how many times we need to repeat
+
+# when we know exactly how many times we need to repeat we use for loop
+
+# for loop is used to iterate over a sequence or range of numbers or collection
+
+for n in range(5):
+    print(n)
+    # we could do more stuff here
+
+# we are outside for loop here
+
+# we could loop over string
+name = "Valdis S"
+for letter in name:
+    print(f"Letter is {letter}")
+
+# if we need an index we can use enumerate
+for index, letter in enumerate(name):
+    print(f"Letter {index} is {letter}")
+    # index and letter are just variables we can use any names here
+# outside loop
+print("We are outside for loop")
+print(f"index is {index}")
+print(f"letter is {letter}")
+
+# we can also loop over a list
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in my_list: # this will be sligthy slower than range
+    print(f"number is {number}")
+
+print("*" * 20)	
+# we can use range with start and stop
+for number in range(5, 10): # note that 10 is not included
+    print(f"number is {number}")
+
+print("*" * 20)
+
+# we can use range with start, stop and step
+for number in range(5, 10, 2): # note that 10 is not included
+    print(f"number is {number}")
+
+# we can loop over multiple collections at the same time using zip
+# zip will stop when the shortest collection is exhausted
+for number, letter in zip(my_list, name):
+    print(f"number is {number} and letter is {letter}")
+
+# we can zip multiple collections
+
+# for loops can use break and continue as well
+
+for number in range(10):
+    if number > 5:
+        print("Breaking out of the loop")
+        print(f"breakout number is {number}")
+        break
+    print(f"number is {number}")
+
+# we can also use continue to skip the rest of the loop
+for n in range(10):
+    if n % 2 == 0:
+        print(f"Skipping even number {n}")
+        continue # we could have use else: here
+    print(f"n is {n}")  
+
+# we can also use else: with for loop
+# else will run only if we did not break out of the loop
+
+for n in range(10):
+    if n % 2 == 0:
+        print(f"Skipping even number {n}")
+        continue # we could have use else: here
+    print(f"n is {n}")
+    if n == 3:
+        print("Breaking out of the loop")
+        break # will cause else to not run
+else: # this else is for for loop NOT if !!
+    print("We did not break out of the loop")
+    print(f"n is {n}")
+
+
+# we can nest loops
+# we can use for loop inside while loop
+# we can use while loop inside for loop
+# we can use for loop inside for loop
+# we can use while loop inside while loop
+
+for x in range(3):
+    # outer block of for loop
+    for y in range(4):
+        # inner block of for loop
+        print(f"x is {x} and y is {y}")
+        print(f"x * y is {x * y}")
+
+# we can use break and continue in nested loops as well
+# break will apply to the innermost loop
+# thus we need two break statements to break out of both loops
