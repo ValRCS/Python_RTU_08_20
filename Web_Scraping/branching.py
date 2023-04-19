9ee6bbcbc0e257c13dd0ca7e9ed8cba2a1847510
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-04-19 18:34:15 +0300

    Boolean logic

--- Web_Scraping/branching.py
+++ Web_Scraping/branching.py
@@ -51,5 +51,52 @@ print(f"Length of {name} {len(name)} < {len(neighbor)} {neighbor}", len(name) < 
 # this is not very useful in our current context
 # will be useful when we use complex data structures
 
+## Boolean Logic
+# Named after George Boole - wikipedia: https://en.wikipedia.org/wiki/George_Boole
+
+# Boolean logic is used to combine multiple boolean values
+
+# simplest is negation using not
+# not True is False
+# not False is True
+print("not True is", not True) # in Python we use English not instead of !
+print("not False is", not False)
+# we can create a toggle with not
+is_spring = not is_spring # so reverse of itself
+print("is_spring is now", is_spring)
+is_spring = not is_spring
+print("is_spring is now", is_spring)
+is_spring = not is_spring
+print("is_spring is now", is_spring)
+is_spring = not is_spring
+print("is_spring is now", is_spring)
+
+# we can have a conjunction using and
+# True and True is True
+# True and False is False
+# False and True is False
+# False and False is False
+print("True and True is", True and True)
+print("True and False is", True and False)
+print("False and True is", False and True)
+print("False and False is", False and False)
+print("is_spring and is_raining", is_spring and is_raining)
+# we can have longer chains of and
+print("True and True and True is", True and True and True)
+# but one False will make the whole thing False
+print("True and True and False is", True and True and False)
+
+# Finally we have disjunction using or
+# True or True is True
+# True or False is True
+# False or True is True
+# False or False is False
+print("True or True is", True or True)
+print("True or False is", True or False)
+print("False or True is", False or True)
+print("False or False is", False or False)
+print("is_spring or is_raining", is_spring or is_raining)
+
+
 
 
