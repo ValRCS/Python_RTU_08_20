d52bc7eaa5b1b1e81a7505d2a8a1e9350c78a21e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-04-19 18:00:16 +0300

    added day 1 from scraping

---
+++ Web_Scraping/hello_scrapers.py
@@ -0,0 +1,24 @@
+print("Hello there web scraper! 😅")
+# Assignments for Day 1
+
+# 1. Create a variable called `name` and assign it your name.
+
+# 2. Print your name five times
+
+# 3. Multiply your name by a number and print the result
+
+# 4. Calculate how many seconds will be this year and print the result
+
+# 5. Print Bananananana using Ba + na * some number
+
+drink = "beer 🍺 "
+
+print(drink * 5)
+# unicode of 🍺 is \U0001F37A
+print(ord("🍺"))
+
+# print unicode for pizza
+print(ord("🍕"))
+
+# lets print pizza and beer 10 times
+print((drink + "🍕") * 10)
