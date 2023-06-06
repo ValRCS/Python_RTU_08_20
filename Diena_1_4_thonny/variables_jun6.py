72d045d65aba75729c7bdbf9cbb5a131bbc6e792
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-06-06 20:23:20 +0300

    updated variables

--- Diena_1_4_thonny/variables_jun6.py
+++ Diena_1_4_thonny/variables_jun6.py
@@ -35,3 +35,34 @@ current_month = 6 # much better than
 c = 6 # much harder to read, is it day what is it?
 this_is_my_current_month_for_my_party = 6 # bad name
 # one,two words are ideal, 3 to 4 are okay
+
+# if we have two different types we can not add them together
+# monster = my_name + a
+# i can use + on same types
+medium_drink = "Fat-free " + drink
+print(f"Cool my new drink {medium_drink}")
+big_drink = "beer " * 5 #  a little trick
+print(big_drink)
+
+# how about adding a number inside string
+# old way
+drink_number = "limonāde " + str(10)
+print(drink_number)
+# f lets us use anything inside {}
+formatted_drink = f"My numbered drink is {drink} I have case with {b} of them"
+print(formatted_drink)
+
+# i can use multiline string
+big_string = """This is a multi line
+text I can do whatever I want
+👨‍🍼 Father’s Day
+"""
+print(big_string)
+
+# i can also format multi-line strings
+my_greeting = f""" I can write whatever
+        My name is {my_name} 
+
+My favorite city is {city}
+"""
+print(my_greeting)
