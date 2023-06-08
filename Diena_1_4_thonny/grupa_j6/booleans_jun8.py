5363bf0fb739f7bff359b721acd06e935aa8a366
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-06-08 19:06:57 +0300

    Moved Files to Folder for J6 group

---
+++ Diena_1_4_thonny/grupa_j6/booleans_jun8.py
@@ -0,0 +1,9 @@
+# Booleans are the most basic data type
+# only 1 or 0 , off or on, True or False
+# so for Booleans we use Capitalized True and False
+is_hot = True
+is_cold = False
+is_summer = True
+print(is_hot, is_cold, is_summer)
+print(f"Is it summer? {is_summer}")
+print(f"{is_summer} is of data type {type(is_summer)}")
