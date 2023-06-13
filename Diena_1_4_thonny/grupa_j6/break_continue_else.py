a033507f409352f503f8cb7856725aa0e89d3ac5
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-06-13 19:08:20 +0300

    else in while loop

--- Diena_1_4_thonny/grupa_j6/break_continue_else.py
+++ Diena_1_4_thonny/grupa_j6/break_continue_else.py
@@ -19,24 +19,24 @@ print("We are free already, but our iterator is now", it)
 
 # we can combine break with regular while condition
 # so we have two ways of exiting the next loop
-total = 0
-counter = 0
-while total < 1_000:
-    number = int(input("Enter an integer please! "))
-    total += number
-    print(f"Total is {total} and entered number is {number}")
-    if counter > 5:
-        print("Sorry only 5 entries allowed")
-        print("Counter", counter)
-        break
-    counter += 1
+# total = 0
+# counter = 0
+# while total < 1_000:
+#     number = int(input("Enter an integer please! "))
+#     total += number
+#     print(f"Total is {total} and entered number is {number}")
+#     if counter > 5:
+#         print("Sorry only 5 entries allowed")
+#         print("Counter", counter)
+#         break
+#     counter += 1
     # i could add other break conditions if I want
 
 # so our loop will continue as long as total stays under 1k
 # and also while counter is 5 or less
-print("We are out of loop")
-print("Counter is", counter)
-print("Total is", total)
+# print("We are out of loop")
+# print("Counter is", counter)
+# print("Total is", total)
 
 # we also have continue instruction which goes back to the start of the loop
 i = 100
@@ -49,3 +49,21 @@ while i < 110:
     print("Things we do when i is not 103", i)
     # could do more stuff for when i is not 103
     # again we could have done this with else
+    
+# finally we have one more construction even more rare than continue
+# else can be attached to while
+# it means while loop finished without break
+
+i = 10
+while i < 15:
+    print("i is", i)
+    i += 1
+    user_input = input("Do you want to continue (y/n)?")
+    if user_input == "n":
+        print("break time")
+        break
+    else: # attached to if, optional
+        print("We are continuing normally")
+else: # we enter this when while loop finished normally
+    print("Finished normally")
+print("Always prints")
