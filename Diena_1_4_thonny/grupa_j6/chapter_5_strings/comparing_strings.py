b157ad9cb772b29e274514405b84a0b0fec192f1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-06-15 18:31:01 +0300

    reading strings

---
+++ Diena_1_4_thonny/grupa_j6/chapter_5_strings/comparing_strings.py
@@ -0,0 +1,5 @@
+# lets ask for user input and compare it with a string
+#
+user_input = input("Please enter a string: ")
+print(f"You entered {user_input}")
+print(f"Your input is of type {type(user_input)}")
