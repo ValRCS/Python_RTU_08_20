411a92dae6a2bc23f717ee34138cdfd64c143eaa
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-06-20 18:37:11 +0300

    prime list solved

--- Diena_1_4_thonny/grupa_j6/chapter_6_lists/ch6_uzd4_prime_list.py
+++ Diena_1_4_thonny/grupa_j6/chapter_6_lists/ch6_uzd4_prime_list.py
@@ -4,15 +4,13 @@ prime_list = [2]
 n = 2
 N = int(input("Lūdzu ievadiet cik pirmskaitļus atrast: "))
  
-while len(prime_list)<N:
+while len(prime_list) < N:
     n+=1 # increase number to be tested
-    is_prime = True
  
     for i in prime_list:
         if n%i==0:
-            is_prime = False
             break # no need to continue testing
-    if is_prime:
+    else: # means for loop finished without break
         prime_list.append(n)
  
 print(prime_list)
