7eef7024af728cd76cc6781405c93d08335e9092
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-07-10 17:55:14 +0300

    update eglite

--- groups/jul4_tue_thu/ch4_looping/uzd2_eglite.py
+++ groups/jul4_tue_thu/ch4_looping/uzd2_eglite.py
@@ -9,5 +9,8 @@ while height <= 0:
         print("Tas nav skaitlis!")
  
 for i in range(height):
+    # spaces = height - i - 1
+    # stars = "*" * (2 * i + 1)
+    # row = " " * spaces + "*" * stars
     row = " " * (height - i - 1) + "*" * (2 * i + 1)
     print(row)
