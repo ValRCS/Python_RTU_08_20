d61e85dec98ee8e0d744111a86f04b669e26e000
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2024-10-09 18:59:44 +0300

    Update food_calculator.py

--- okt724/food_calculator.py
+++ okt724/food_calculator.py
@@ -32,8 +32,33 @@ print(f"So would 2kg price for {food} be {price_2kg}?")
 # float - floating point, has commas, precise for around 15 digits after comma
 # we can attempt to convert string to float with float function
 price_float = float(price) # I added _float for education purposed but could be any other name
+# note that we depend on user to provide valid number
 price_3kg = price_float * 3
 print("Now let's try this again")
 print(f"Then 3kg of {food} should be {price_3kg}")
 
-
+# now let's ask how much food they
+quantity = input(f"How many kg of {food} do you want to buy {name}?")
+print(f"Wow you want to buy {quantity} kg of {food}")
+# we need quantity to be number
+# let's assume we only allow full numbers - integers
+# then lets convert quantity
+# I will overwrite the old quantity with new quantity
+quantity = int(quantity) # again will fail on non integers
+# now we are almost ready to calculate
+# first let's print data types
+# usually we do not need to print them, but good for debugging purposes
+print(f"name is {name} and its data type is {type(name)}")
+print(f"price is {price} and its data type is {type(price)}")
+print(f"price_float is {price_float} and its data type is {type(price_float)}")
+print(f"quantity is {quantity} and its data type is {type(quantity)}")
+# now let's calculate the total from price and quantity
+total = price_float * quantity
+print(f"Alrighty {name}! So {quantity} kg of {food} would be {total}")
+# it is possible the total would not be so nice due to floats
+# we might want to round it up a bit
+total_int = int(total) # this will round down guaranteed to integer
+total = round(total, 2) # 2 digits after comma, we lose all the extra digits
+# old value of total was overwritten by rounded version 
+print(f"So {total} price while integer portion is {total_int} Euros")
+# there are a couple more primite data types for next lesson
