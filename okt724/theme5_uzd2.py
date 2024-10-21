4f6ab689de57a1dd2b53c654ed3f26f6840335fc
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-10-21 18:59:39 +0300

    working hangman

--- okt724/theme5_uzd2.py
+++ okt724/theme5_uzd2.py
@@ -6,6 +6,8 @@
 # Kartupeļu lauks -> ********* *****
 # ievada a -> *a****** *a***
 # Principā tas ir labs iesākums karātavu spēlei.
+MAX_BAD_GUESS = 5
+
 player1_str = input("Ievadiet tekstu ->")
 star_str = '' # in beginning new_str is empty
 for original_ch in player1_str: # for each character in player1_str
@@ -27,29 +29,41 @@ print("Slēptais teksts: " + star_str)
 #             guessed_str += ' '
 #     else:
 #          guessed_str += '*'
+bad_guess_count = 0 # initialize the number of guesses
 
 while player1_str != star_str: # we continue until the guessed string is the same as the original string
+    if bad_guess_count >= MAX_BAD_GUESS: # if we have reached the maximum number of guesses
+        print("Jūs zaudējāt")
+        print(f"Jūs minējāt {bad_guess_count} reizes")	
+        print("Vārds bija: " + player1_str)	
+        break
     player2_guess = input("Ievadiet lūdzu simbolu ->") # we ask for a character
     buffer = '' # we reset the guessed string
     for original_ch, hidden_ch in zip(player1_str, star_str): # we go through the original string
         if original_ch == player2_guess: # if the character is the same as the one guessed
             buffer += original_ch # then we add the character to the guessed string
-        # elif original_ch == ' ': # if we have space
-        #     buffer += ' ' # then we add space to the guessed string
-        # we do not need to check for space, because we have already added it to the guessed string
         else: # otherwise we add a star
             buffer += hidden_ch # we add the hidden character
-    # finally we need to assign the buffer to the guessed string
+            
     star_str = buffer # critical otherwise we lose all the previous guesses
+    # let's check if we should increment the bad_guess_count
+    if player2_guess not in player1_str: #not the most efficient way but okay for short strings
+        bad_guess_count += 1
     print(f"Ievadītais simbols {player2_guess} tekstā "+ star_str)
-# we do not have to use range to iterate through the string
-# for ch in player1_str: # so we go through the original string one by one
-#     # we have three possible cases
-#     # one is when the character is the same as the one guessed
-#     if ch == player2_guess: 
-#         guessed_str += ch # then we add the character to the guessed string
-#     elif ch == ' ': # we have space
-#         guessed_str += ' ' # then we add space to the guessed string
-#     else:# otherwise we add a star
-#         guessed_str += '*' 
-print(f"Ievadītais simbols {player2_guess} tekstā "+ buffer)
+
+# check winner
+if bad_guess_count < MAX_BAD_GUESS:
+    print("Jūs uzvarējāt")
+    print(f"Jūs minējāt {bad_guess_count} reizes")	
+    print("Vārds bija: " + player1_str)
+
+# TODO 
+# idejas uzlabojumiem
+# pielikt kopējo skaitītāju, cik reizes minēts
+# rādīt, cik reizes vēl var minēt
+# radīt vizuāli cilvēciņu, vai kā savādāk
+# vēlāk varētu veidot spēli cilvēks pret datoru
+# dators piedāvātu vārdu no saraksta, vai no kāda ārēja resursa (tīmekļa)
+# iespējams arī var paņemt vārdus no kāda teksta faila (to arī skatīsimies citā nodarbībā)
+
+
