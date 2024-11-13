9e66b422348076702e9a00461990785ff9a7888f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-11-13 19:02:25 +0200

    added simple compilation example

---
+++ okt724/theme_16_road_ahead/which_python.py
@@ -0,0 +1,5 @@
+# let's write a short script which shows which python we are using
+import sys
+print(f"Python version: {sys.version}")
+print(f"Python executable: {sys.executable}")
+print(f"Python path: {sys.path}")
